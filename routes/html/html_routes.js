8751b7649d563b3fae0f55b8ea08268229d6fa38
Sebastian Brear <ssbrear@ucdavis.edu>
2020-10-10 11:57:04 -0700

    home, login, signup, and dashboard are all interlinked and the user can create an account, sign in, and logout

--- routes/html/html_routes.js
+++ routes/html/html_routes.js
@@ -9,28 +9,28 @@ var db = require("../../models/");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
-
+module.exports = function (app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
+  app.get("/", checkNotAuth, function (req, res) {
     res.render("home");
   });
 
-  app.get("/login", function(req, res) {
+  app.get("/login", function (req, res) {
     res.render("login");
   });
 
-  app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
+    req.logout();
     res.redirect("/");
   });
 
-  app.get("/signup", function(req, res) {
-    res.render("signup")
+  app.get("/signup", function (req, res) {
+    res.render("signup");
   });
 
-  app.get("/dashboard", function(req, res) {
+  app.get("/dashboard", checkAuth, function (req, res) {
     db.Stock.findAll({}).then((data) => {
       console.log(data);
       let stocks = [];
@@ -38,7 +38,21 @@ module.exports = function(app) {
         stocks.push(element.dataValues);
       });
       console.log(stocks);
-      res.render("dashboard", {stocks: stocks});
-    })
-  })
-}
+      res.render("dashboard", { stocks: stocks });
+    });
+  });
+
+  // Middleware to not allow access to the list without being signed in
+  function checkAuth(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect("/");
+  }
+  function checkNotAuth(req, res, next) {
+    if (req.isAuthenticated()) {
+      res.redirect("/dashboard");
+    }
+    return next();
+  }
+};

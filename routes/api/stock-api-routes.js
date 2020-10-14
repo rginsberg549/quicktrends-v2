9e49dd824ab9c249d1d2dfa798035d81fe01095d
rginsberg <rginsberg549@gmail.com>
2020-10-14 01:30:04 -0700

    EPS, Gross Profit Ratio, Net Income Ratio are all displaying the template

--- routes/api/stock-api-routes.js
+++ routes/api/stock-api-routes.js
@@ -41,8 +41,26 @@ module.exports = function (app) {
       params: {
         apikey: apikey,
       },
-    })
-      .then((response) => {
+    }).then((profile) => {
+      console.log(profile);
+
+      axios({
+        method: "GET",
+        url:
+          "https://financial-modeling-prep.p.rapidapi.com/income-statement/" +
+          stockSymbol +
+          "?limit=1",
+        headers: {
+          "content-type": "application/octet-stream",
+          "x-rapidapi-host": "financial-modeling-prep.p.rapidapi.com",
+          "x-rapidapi-key":
+            "c23481d564msh4d48ca2d97c6375p1be85ejsna769421f074b",
+          useQueryString: true,
+        },
+        params: {
+          apikey: apikey,
+        },
+      }).then((incomeStatement) => {
         db.Stock.findOne({
           where: {
             user_id: req.user.id,
@@ -54,13 +72,16 @@ module.exports = function (app) {
             db.Stock.create({
               name: stockSymbol,
               user_id: req.user.id,
-              price: response.data[0].price,
-              lastDiv: response.data[0].lastDiv,
-              companyName: response.data[0].companyName,
-              website: response.data[0].website,
-              ceo: response.data[0].ceo,
-              sector: response.data[0].sector,
-              image: response.data[0].image,
+              price: profile.data[0].price,
+              lastDiv: profile.data[0].lastDiv,
+              companyName: profile.data[0].companyName,
+              website: profile.data[0].website,
+              ceo: profile.data[0].ceo,
+              sector: profile.data[0].sector,
+              image: profile.data[0].image,
+              eps: incomeStatement.data[0].eps,
+              grossProfitRatio: incomeStatement.data[0].grossProfitRatio,
+              netIncomeRatio: incomeStatement.data[0].netIncomeRatio
             }).then(function (dbStock) {
               return res.json(dbStock);
             });
@@ -75,6 +96,9 @@ module.exports = function (app) {
                 ceo: response.data[0].ceo,
                 sector: response.data[0].sector,
                 image: response.data[0].image,
+                eps: incomeStatement.data[0].eps,
+                grossProfitRatio: incomeStatement.data[0].grossProfitRatio,
+                netIncomeRatio: incomeStatement.data[0].netIncomeRatio
               },
               {
                 where: {
@@ -96,19 +120,20 @@ module.exports = function (app) {
             });
           }
         });
-      })
-      .catch((error) => {
-        console.log(error);
       });
+    });
   });
-
+  
+  
   app.delete("/api/stocks/:id", function (req, res) {
     db.Stock.destroy({
       where: {
         id: req.params.id,
       },
-    }).then(function (dbStock) {
-      res.json(dbStock);
-    });
-  });
-};
+    })
+    .then(function (dbStock) {
+    res.json(dbStock);
+  })
+});
+
+}
